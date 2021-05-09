5360818bda5382306a2499edb15e08e505b102e0
gina-cyber <77301888+gina-cyber@users.noreply.github.com>
2021-05-09 12:42:03 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,1 +1,48 @@
 
+#from Unit 5 Notes with Megan:
+
+#from flask import Flask, #render-template()
+
+#app = Flask(__name__)
+
+#@app.route('/')
+      #(or @app.route('/meow')
+#def index():
+  #return 'Hello from Pet Adoption' #After: return render_template('index.html') (...This sends message to browser to add hyper link)
+    #(or return 'meow')
+    #replace with html site instead of the single message above: OR: return'''
+       #<h1>Pet Adoption</h1>
+       #<button>Add Pet</button>
+        #'''
+  
+  
+  #if __name__== '__main__':
+    #app.run(debug=True, part=8000, host='0.0.0.0')
+    #(CLick on top eye and matching port to open up website and see what was typed on workspace)
+    
+    #___________________________________
+    #UNDER index.html folder:
+    #<!DOCTYPE html>
+    #<html>
+      #<head>
+           #<meta charset="utf-8>
+           #<title>Pet Adoption</title>
+           #<meta name="description" content="A collection ofadpted pets"
+           #<meta name="viewport" content="width=device-width, initi..  
+      #</head>
+      #<body>
+          #<section>
+               #<a href="#"></a>
+                   #h1>Pet Adoption</h1>
+               #</a>
+          #</section>
+          ##<section>
+            #a href="#">
+               #<div>
+                    #<h2>Pet's name</h2>
+                    #<p>Age</p>
+                    #<p>Des</p>
+                    
+      #</body>
+      #</body>
+    #</html>
